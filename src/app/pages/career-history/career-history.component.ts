abe5e2edb91d256aa0c18a82e4bfa28d72a58019
Divaldo Aderaldo de Oliveira Júnior <divaldojr9@gmail.com>
2024-05-31 13:33:06 -0300

    Career History - Company URLs

--- src/app/pages/career-history/career-history.component.ts
+++ src/app/pages/career-history/career-history.component.ts
@@ -16,12 +16,14 @@ export class CareerHistoryComponent extends CurrentLanguageChecker {
       'pt-BR': {
         title: 'Desenvolvedor Web Full Stack Freelance',
         company: 'Azulejus',
+        companyURL: 'https://www.instagram.com/azulejus/',
         startDate: 'Julho/2023',
         description: 'Desenvolvimento e manutenção de uma aplicação web de gerenciamento de vendas, clientes, e estoque utilizando as tecnologias Angular, NestJS, Mongoose e MongoDB.'
       },
       'en-US': {
         title: 'Full Stack Freelance Web Developer',
         company: 'Azulejus',
+        companyURL: 'https://www.instagram.com/azulejus/',
         startDate: 'July/2023',
         description: 'Development and maintenance of a web application for managing sales, customers, and inventory using Angular, NestJS, Mongoose, and MongoDB.'
       }
@@ -30,6 +32,7 @@ export class CareerHistoryComponent extends CurrentLanguageChecker {
       'pt-BR': {
         title: 'Desenvolvedor Web Front-End',
         company: 'Pathfind',
+        companyURL: 'https://www.pathfind.com.br/',
         startDate: 'Julho/2019',
         endDate: 'Abril/2023',
         description: 'Desenvolvimento front-end de uma aplicação web desktop utilizando Angular, incluindo criação de telas seguindo designs web pré-definidos, conexão de front-end e endpoints API no back-end, implementação de funcionalidades para a formatação e utilização de dados recebidos dos endpoints, e geração de testes unitários.'
@@ -37,6 +40,7 @@ export class CareerHistoryComponent extends CurrentLanguageChecker {
       'en-US': {
         title: 'Front-End Web Developer',
         company: 'Pathfind',
+        companyURL: 'https://www.pathfind.com.br/',
         startDate: 'July/2019',
         endDate: 'April/2023',
         description: 'Front-end development of a desktop web application using Angular, including creating screens following pre-defined web designs, connecting front-end to API endpoints in the back-end, implementing functionalities for formatting and utilizing data received from endpoints, and creation of unit tests.'
